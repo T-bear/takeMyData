9d402c02f26a4a934ea1132e74febe3d121a9488
TommiSvard <tommi.svard@hotmail.se>
2017-10-17 16:19:12 +0200

    page after slow is chosen

---
+++ Load.js
@@ -0,0 +1,23 @@
+function move() {
+    var elem = document.getElementById("myBar"); 
+    var width = 1;
+    var id = setInterval(frame, 200);
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        }else if (width === 99) {
+            setTimeout(function() {}, 10);
+            width = 100;
+        }
+
+        else {
+            width++; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width * 1 + '%';
+
+        }
+    }
+
+}
+
